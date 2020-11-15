f7f0880a0deb98ca8d38416152308cc4fd05d704
jparlato <jparlato@mindspring.com>
2020-11-15 01:42:24 -0500

    bind to class

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,19 +14,29 @@ namespace AppSettingsManager.Controllers
 	{
 		private readonly ILogger<HomeController> _logger;
 		private readonly IConfiguration _config;
+		private TwilioSettings _twilioSettings;
+
 
 		public HomeController(ILogger<HomeController> logger, IConfiguration config)
 		{
 			_logger = logger;
 			_config = config;
+			_twilioSettings = new TwilioSettings();
+			config.GetSection("Twilio").Bind(_twilioSettings);
+
 
 		}
 
 		public IActionResult Index()
 		{
 			ViewBag.SendGridKey = _config.GetValue<string>("SendGridKey");
-			ViewBag.TwilioAuthToken = _config.GetValue<string>("Twilio:Authtoken");
+			ViewBag.TwilioAuthToken = _config.GetSection("Twilio").GetValue<string>("Authtoken");
 			ViewBag.TwilioAccountSid = _config.GetValue<string>("Twilio:AccountSid");
+			ViewBag.TwilioPhoneNumber = _twilioSettings.PhoneNumber;
+
+			//ViewBag.BottomLevelSetting = _config.GetSection("FirstLevelSetting").GetSection("SecondLevelSetting").GetValue<string>("BottomLevelSetting");
+			//ViewBag.BottomLevelSetting = _config.GetValue<string>("FirstLevelSetting:SecondLevelSetting:BottomLevelSetting");
+			ViewBag.BottomLevelSetting = _config.GetSection("FirstLevelSetting").GetSection("SecondLevelSetting").GetSection("BottomLevelSetting").Value;
 
 			return View();
 		}

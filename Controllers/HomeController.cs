70bb2b46520056bd5ddcf3a8f460a8a81bfe9d64
jparlato <jparlato@mindspring.com>
2020-11-15 01:55:14 -0500

    Use start up to configure appsettings class access

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AppSettingsManager.Models;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace AppSettingsManager.Controllers
 {
@@ -15,14 +16,17 @@ namespace AppSettingsManager.Controllers
 		private readonly ILogger<HomeController> _logger;
 		private readonly IConfiguration _config;
 		private TwilioSettings _twilioSettings;
+		private readonly IOptions<TwilioSettings> _twilioOptions;
 
 
-		public HomeController(ILogger<HomeController> logger, IConfiguration config)
+		public HomeController(ILogger<HomeController> logger, IConfiguration config, IOptions<TwilioSettings> twilioOptions)
 		{
 			_logger = logger;
 			_config = config;
 			_twilioSettings = new TwilioSettings();
-			config.GetSection("Twilio").Bind(_twilioSettings);
+			//config.GetSection("Twilio").Bind(_twilioSettings);
+			_twilioOptions = twilioOptions;
+
 
 
 		}
@@ -30,9 +34,13 @@ namespace AppSettingsManager.Controllers
 		public IActionResult Index()
 		{
 			ViewBag.SendGridKey = _config.GetValue<string>("SendGridKey");
-			ViewBag.TwilioAuthToken = _config.GetSection("Twilio").GetValue<string>("Authtoken");
-			ViewBag.TwilioAccountSid = _config.GetValue<string>("Twilio:AccountSid");
-			ViewBag.TwilioPhoneNumber = _twilioSettings.PhoneNumber;
+			//ViewBag.TwilioAuthToken = _config.GetSection("Twilio").GetValue<string>("Authtoken");
+			//ViewBag.TwilioAccountSid = _config.GetValue<string>("Twilio:AccountSid");
+			//ViewBag.TwilioPhoneNumber = _twilioSettings.PhoneNumber;
+
+			ViewBag.TwilioAuthToken = _twilioOptions.Value.AuthToken;
+			ViewBag.TwilioAccountSid = _twilioOptions.Value.AccountSid;
+			ViewBag.TwilioPhoneNumber = _twilioOptions.Value.PhoneNumber;
 
 			//ViewBag.BottomLevelSetting = _config.GetSection("FirstLevelSetting").GetSection("SecondLevelSetting").GetValue<string>("BottomLevelSetting");
 			//ViewBag.BottomLevelSetting = _config.GetValue<string>("FirstLevelSetting:SecondLevelSetting:BottomLevelSetting");
